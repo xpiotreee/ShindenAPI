be0a16d19b80219ea6518ebec856de9b9b751bbb
xpiotreee <piotrowski.00@wp.pl>
2022-02-15 19:22:49 +0100

    accept-language header update

--- shinden/__init__.py
+++ shinden/__init__.py
@@ -16,7 +16,7 @@ class Shinden():
         self._http = ClientSession(
             headers={
                 'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.93 Safari/537.36.',
-                'Accept-Language': 'pl,en-US;q=0.9,en;q=0.8'
+                'Accept-Language': 'pl'
             }
         )
 
